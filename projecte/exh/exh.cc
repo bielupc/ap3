4a4179906564a3dddc77148b045fe4a5a64d1e64
bielupc <biel.altimira@estudiantat.upc.edu>
2023-11-30 23:49:23 +0100

    exh funcinant

--- projecte/exh/exh.cc
+++ projecte/exh/exh.cc
@@ -18,45 +18,45 @@ public:
   int points;
 
   Player(int ident, const string &n, const string &pos, int pr, const string &c,
-         int p)
-      : id(ident), name(n), position(pos), price(pr), club(c), points(p) {}
+         int p):
+         id(ident), name(n), position(pos), price(pr), club(c), points(p) {}
 };
 
 class Tactic {
 public:
-  int por = -1;
-  vector<int> def;
-  vector<int> mig;
-  vector<int> dav;
+  string por;
+  vector<string> def;
+  vector<string> mig;
+  vector<string> dav;
   int points;
   int price;
 
-  Tactic() : por(-1), points(0), price(0) {}
-  Tactic(int por, vector<int> &def, vector<int> &mig, vector<int> &dav,
+  Tactic() : points(0), price(0) {}
+  Tactic(string por, vector<string>& def, vector<string>& mig, vector<string>& dav,
          int points, int price)
       : por(por), def(def), mig(mig), dav(dav), points(points), price(price) {}
 
-  void showTactic() {
-    cout << "Por: " << this->por << endl;
-    cout << "Def: ";
-    for (int p : this->def) {
-      cout << p << " ";
-    }
-    cout << endl;
-    cout << "Mig: ";
-    for (int p : this->mig) {
-      cout << p << " ";
-    }
-    cout << endl;
+  // void showTactic() {
+  //   cout << "Por: " << this->por << endl;
+  //   cout << "Def: ";
+  //   for (string p : this->def) {
+  //     cout << p << " ";
+  //   }
+  //   cout << endl;
+  //   cout << "Mig: ";
+  //   for (string p : this->mig) {
+  //     cout << p << " ";
+  //   }
+  //   cout << endl;
 
-    cout << "Dav: ";
-    for (int p : this->dav) {
-      cout << p << " ";
-    }
-    cout << endl;
-    cout << "Points: " << this->points << endl;
-    cout << "Price: " << this->price << endl;
-  }
+  //   cout << "Dav: ";
+  //   for (string p : this->dav) {
+  //     cout << p << " ";
+  //   }
+  //   cout << endl;
+  //   cout << "Points: " << this->points << endl;
+  //   cout << "Price: " << this->price << endl;
+  // }
 };
 
 using Players = vector<Player>;
@@ -67,20 +67,19 @@ Players porters;
 Players defenses;
 Players migcampistes;
 Players davanters;
-int MAX_POINTS = 0;
+int MAX_POINTS = -1;
 
-string print_players(const string label, const vector<int> &ids,
-                     Players &players) {
+string print_players(const string label, const vector<string>& names) {
   stringstream phrase;
   phrase << label << ": ";
   bool first = true;
-  for (int id : ids) {
+  for (string name: names) {
     if (first) {
-      phrase << players[id].name;
+      phrase << name;
       first = false;
     } else {
       phrase << ";";
-      phrase << players[id].name;
+      phrase << name;
     }
   }
   return phrase.str();
@@ -91,10 +90,12 @@ void write(Tactic &t, const char *output) {
   if (file.is_open()) {
 
     // output << fixed << setprecision(1) << duration.count() << endl;
-    file << "POR: " << porters[t.por].name << endl;
-    file << print_players("DEF", t.def, defenses) << endl;
-    file << print_players("MIG", t.mig, migcampistes) << endl;
-    file << print_players("DAV", t.dav, davanters) << endl;
+    file << "2.3" << endl;
+
+    file << "POR: " << t.por<< endl;
+    file << print_players("DEF", t.def) << endl;
+    file << print_players("MIG", t.mig) << endl;
+    file << print_players("DAV", t.dav) << endl;
     file << "Punts: " << t.points << endl;
     file << "Preu: " << t.price << endl;
     file.close();
@@ -107,38 +108,39 @@ void write(Tactic &t, const char *output) {
 void exh(Tactic &t, const char *output, int idx, bool fullPor, bool fullDef,
          bool fullMig, bool fullDav) {
 
-  // test
-  if (fullDav) {
-    // MAX_POINTS = t.points;
-    t.showTactic();
-    cout << "________________" << endl;
+  if(t.price > T){
     return;
   }
 
-  if (fullPor and fullDef and fullMig and fullDav and t.points > MAX_POINTS) {
+  if (fullDav and t.points > MAX_POINTS) {
+    // auto end = high_resolution_clock::now();
+    // duration<double> duration = end - startTimer;
+    MAX_POINTS = t.points;
     write(t, output);
     return;
   }
 
   if (!fullPor) {
-    for (Player p : porters) {
-      t.por = p.id;
+    for (const Player& p : porters) {
+      t.por = p.name;
       t.price += p.price;
       t.points += p.points;
+
+      // auto startTimer = high_resolution_clock::now();
       exh(t, output, 0, true, false, false, false);
 
       t.price = 0;
       t.points = 0;
     }
   } else if (!fullDef) {
-    if (idx > defenses.size())
+    if (idx > defenses.size() - 1)
       return;
 
     if (t.def.size() == N1) {
-      exh(t, output, 0, true, true, false, false);
+      exh(t, output, 0,  true, true, false, false);
     }
     else{
-      t.def.push_back(defenses[idx].id);
+      t.def.push_back(defenses[idx].name);
       t.price += defenses[idx].price;
       t.points += defenses[idx].points;
       exh(t, output, idx + 1, true, false, false, false);
@@ -149,13 +151,13 @@ void exh(Tactic &t, const char *output, int idx, bool fullPor, bool fullDef,
       exh(t, output, idx + 1, true, false, false, false);
     }
   } else if (!fullMig) {
-    if (idx > migcampistes.size())
+    if (idx > migcampistes.size() - 1)
       return;
 
     if (t.mig.size() == N2) {
       exh(t, output, 0, true, true, true, false);
     }else{
-      t.mig.push_back(migcampistes[idx].id);
+      t.mig.push_back(migcampistes[idx].name);
       t.price += migcampistes[idx].price;
       t.points += migcampistes[idx].points;
       exh(t, output, idx + 1, true, true, false, false);
@@ -166,13 +168,13 @@ void exh(Tactic &t, const char *output, int idx, bool fullPor, bool fullDef,
       exh(t, output, idx + 1, true, true, false, false);
     }
   } else if (!fullDav) {
-    if (idx > davanters.size())
+    if (idx > davanters.size() - 1)
       return;
 
     if (t.dav.size() == N3) {
       exh(t, output, 0, true, true, true, true);
     }else{
-      t.dav.push_back(davanters[idx].id);
+      t.dav.push_back(davanters[idx].name);
       t.price += davanters[idx].price;
       t.points += davanters[idx].points;
       exh(t, output, idx + 1, true, true, true, false);
@@ -243,11 +245,11 @@ int main(int argc, char const *argv[]) {
   }
 
   // Executes exhaustive search
-  // auto start = high_resolution_clock::now();
   Tactic tactic;
+
+
+  
   exh(tactic, argv[3], 0, false, false, false, false);
-  // auto end = high_resolution_clock::now();
-  // duration<double> duration = end - start;
 
   return 0;
 }
--- projecte/exh/todo.md
+++ projecte/exh/todo.md
@@ -1,10 +1,11 @@
-[ ] Checker no passa però format correcte
+[X] Checker no passa però format correcte
 [ ] Cronometrar cada solucio
 [ ] Una solució és millor si ha tardat menys?
-[ ] Print dels jugadors amb el nou sistema de ID's.
+[X] Print dels jugadors amb el nou sistema de ID's.
+[ ] Unificar vectors.
 
 
 [X] Al llegir, podem agafar només els jugadors amb preu < J
 [ ] Eliminar paramestres de exh amb ifs de size dels vectors players?
 [ ] Funcio per la generacio de def, dav, mig?
-[ ] Pot retornar una solució incompleta al arribar al maxim index?
+[X] Pot retornar una solució incompleta al arribar al maxim index?
